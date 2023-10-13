334430a1d255bd2a3fb2252ea29a7fdf43c3a953
Arnesh Saha <s2223898@ed.ac.uk>
2023-10-13 20:43:39 +0100

    Commented code - seems to be working

--- src/main/java/uk/ac/ed/inf/Main.java
+++ src/main/java/uk/ac/ed/inf/Main.java
@@ -1,44 +1,13 @@
 package uk.ac.ed.inf;
 
 import uk.ac.ed.inf.ilp.constant.OrderStatus;
-import uk.ac.ed.inf.ilp.data.LngLat;
-import uk.ac.ed.inf.ilp.data.NamedRegion;
-import uk.ac.ed.inf.ilp.data.Order;
-import uk.ac.ed.inf.ilp.data.Restaurant;
-import uk.ac.ed.inf.ilp.interfaces.*;
-public class Main implements LngLatHandling, OrderValidation
-{
+
+public class Main {
     public static void main( String[] args )
     {
         System.out.println( "Small test for the IlpDataObjects!" );
         OrderStatus orderStatus = OrderStatus.DELIVERED;
         System.out.println(orderStatus);
         System.out.println("IlpDataObjects.jar is working!");
-    }
-
-    @Override
-    public double distanceTo(LngLat startPosition, LngLat endPosition) {
-        return 0;
-    }
-
-    @Override
-    public boolean isCloseTo(LngLat startPosition, LngLat otherPosition) {
-        return false;
-    }
-
-    @Override
-    public boolean isInRegion(LngLat position, NamedRegion region) {
-//        Raycasting algorithm
-        return false;
-    }
-
-    @Override
-    public LngLat nextPosition(LngLat startPosition, double angle) {
-        return null;
-    }
-
-    @Override
-    public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants) {
-        return null;
     }
 }
