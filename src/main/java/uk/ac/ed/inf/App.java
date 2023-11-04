3de389736c5ca5960614bea9cfb3f7953adc88f6
Arnesh Saha <s2223898@ed.ac.uk>
2023-11-04 23:54:46 +0000

    OrderValidation with REST fetch done -- TODO: pathfinding

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,43 +1,52 @@
-package uk.ac.ed.inf.Data;
+package uk.ac.ed.inf;
 
 import uk.ac.ed.inf.ilp.data.Order;
 import uk.ac.ed.inf.controller.DeliveryController;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 public class App {
 
-    public static void main(String[] args) {
-        // Check if the correct number of arguments are passed
-        if (args.length != 2) {
-            System.out.println("Usage: java -jar PizzaDronz-1.0-SNAPSHOT.jar <date> <REST-server-URL>");
-            System.exit(1);
+    public static void main(String[] args){
+        // check that there are the correct number of arguments
+        if (args.length != 2){
+            System.err.println("Incorrect number of arguments, expected 2 got " + args.length);
+        } else {
+            // set the url and date
+            var date = args[0];
+            var url = args[1];
+            // validate the date and url
+            try {
+                LocalDate parsed = LocalDate.parse(date);
+                if (isValidURL(url)){
+                    // construct the deliverorders object, and start execution
+//                    TODO: uncomment this line
+//                    DeliveryController main = new DeliveryController(date, url);
+//                    main.run();
+                }
+            } catch (DateTimeParseException e){
+                System.err.println(e);
+                System.err.println("Invalid date.");
+            } catch (MalformedURLException | URISyntaxException e){
+                System.err.println(e);
+                System.err.println("Invalid url.");
+            }
         }
+    }
 
-        // Get the date and REST server URL from command-line arguments
-        String date = args[0];
-        String baseUrl = args[1];
-
-        // Instantiate the DeliveryHandler with the base URL
-        DeliveryController deliveryController = new DeliveryController(baseUrl);
-
-        // Fetch orders for the specified date
-        List<Order> orders = deliveryController.getOrdersForDate(date);
-        if (orders == null) {
-            System.out.println("Failed to fetch orders or no orders found for the specified date.");
-            System.exit(2);
+    private static boolean isValidURL(String url) throws MalformedURLException, URISyntaxException {
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (MalformedURLException e) {
+            return false;
+        } catch (URISyntaxException e) {
+            return false;
         }
-
-        // Validate the orders
-        boolean areOrdersValid = deliveryController.validateOrders(orders);
-        if (!areOrdersValid) {
-            System.out.println("Validation failed for one or more orders.");
-            System.exit(3);
-        }
-
-        // If validation passes, proceed with further processing (not shown)
-        // ...
-
-        System.out.println("All orders for " + date + " are valid.");
     }
 }
